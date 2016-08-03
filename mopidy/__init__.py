4efb4b27855ef006bfe9476746bb1e6636cc136a
Si P <swilso793@gmail.com>
2016-08-03 17:50:22 +0200

    added some more functions to music control

--- mopidy/__init__.py
+++ mopidy/__init__.py
@@ -19,8 +19,33 @@ class MusicSkill(MycroftSkill):
 		play_intent = IntentBuilder("PlayIntent").require("PlayMusic").build()
                 self.register_intent(play_intent, self.handle_play_intent)
 
+		pause_intent = IntentBuilder("PauseIntent").require("PauseMusic").build()
+                self.register_intent(pause_intent, self.handle_pause_intent)
+
+		next_song_intent = IntentBuilder("NextSongIntent").require("NextSong").build()
+                self.register_intent(next_song_intent, self.handle_next_song_intent)
+
+		lower_volume_intent = IntentBuilder("LowerVolumeIntent").require("LowerVolume").build()
+                self.register_intent(lower_volume_intent, self.handle_lower_volume_intent)
+
+		raise_volume_intent = IntentBuilder("RaiseVolumeIntent").require("RaiseVolume").build()
+                self.register_intent(raise_volume_intent, self.handle_raise_volume_intent)
+
+
+
 	def handle_play_intent(self, message):	
 		os.system('mpc play')	
+	def handle_pause_intent(self, message):	
+		os.system('mpc pause')	
+	def handle_next_song_intent(self, message):	
+		os.system('mpc next')	
+	
+	def handle_lower_volume_intent(self, message):	
+		os.system('mpc volume -25')	
+
+	def handle_raise_volume_intent(self, message):	
+		os.system('mpc volume +25')	
+
 	def stop(self):
     		pass
 
