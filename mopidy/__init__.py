d58daf9e166426734218997e83a96f04513de673
Si P <swilso793@gmail.com>
2016-08-05 21:49:57 +0200

    play a song

--- mopidy/__init__.py
+++ mopidy/__init__.py
@@ -1,5 +1,7 @@
+import time
 from os.path import dirname
 import os
+import subprocess
 import binascii 
 from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill
@@ -12,9 +14,9 @@ class MusicSkill(MycroftSkill):
 	def __init__(self):
 		super(MusicSkill, self).__init__(name="MusicSkill")
 
-
 	def initialize(self):
     		self.load_data_files(dirname(__file__))
+		self.load_regex_files(dirname(__file__))
 
 		play_intent = IntentBuilder("PlayIntent").require("PlayMusic").build()
                 self.register_intent(play_intent, self.handle_play_intent)
@@ -30,7 +32,9 @@ class MusicSkill(MycroftSkill):
 
 		raise_volume_intent = IntentBuilder("RaiseVolumeIntent").require("RaiseVolume").build()
                 self.register_intent(raise_volume_intent, self.handle_raise_volume_intent)
-
+		
+		find_add_intent = IntentBuilder("FindAddIntent").require("Play").optionally("Songname").build()
+                self.register_intent(find_add_intent, self.handle_find_add_intent)
 
 
 	def handle_play_intent(self, message):	
@@ -45,6 +49,18 @@ class MusicSkill(MycroftSkill):
 
 	def handle_raise_volume_intent(self, message):	
 		os.system('mpc volume +25')	
+
+	def handle_find_add_intent(self, message):
+		nameUtterance = message.metadata.get("Songname", None)
+		
+		find_query = "mpc find any " + "'" + nameUtterance + "'"
+		result = subprocess.check_output(find_query, shell=True)
+		a = result.split('\n')[0]	
+		find_add_query = "mpc findadd any " + "'" + a[14:] + "'" 
+		os.system(find_add_query)
+		os.system('mpc play')
+		time.sleep(2)
+		os.system('mpc crop')
 
 	def stop(self):
     		pass
